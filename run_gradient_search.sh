8678a38b78538bee11c7838c9c0381646e2fe834
JustPhyCs <hyli16@stanford.edu>
2018-08-06 20:33:35 -0700

    minor

--- run_gradient_search.sh
+++ run_gradient_search.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env bash
-
+bsub -q psanaq -n 24 -R"span[ptile=1]" -o %J.out mpirun --mca btl ^openib \
 python /reg/neh/home5/haoyuan/Documents/my_repos/Alignment/gradient_search.py \
 --output "/reg/d/psdm/amo/amo86615/res/haoyuan/alignment/output" \
 --fixed_target "/reg/d/psdm/amo/amo86615/res/haoyuan/alignment/input/chop_complete.npy" \
---movable_target "/reg/d/psdm/amo/amo86615/res/haoyuan/alignment/input/chop_category_0.npy" \
---tag "cat_0"
+--movable_target "/reg/d/psdm/amo/amo86615/res/haoyuan/alignment/input/chop_category_2.npy" \
+--iter_num 9 \
+--tag "cat_2"
